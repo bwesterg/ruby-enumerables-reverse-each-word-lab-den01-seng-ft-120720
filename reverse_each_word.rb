23172cfc52c79b56a095ccd0ce8503ab0a4b6ab7
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-29 00:56:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+#require "pry"
+
+def reverse_each_word(sentence)
+  sentence.split.map {|word| word.reverse}.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+    sentence.split.collect {|word| word.reverse}.join(" ")
+end
