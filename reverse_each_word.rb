ab2543d3ed18652b815185c6e243508147969a1e
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-29 00:51:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 #require "pry"
 
 def reverse_each_word(sentence)
-  #new_array = sentence.split(" ")
-
   sentence.split.map {|word| word.reverse}.join(" ")
 end
 
 
-
+def reverse_each_word(sentence)
+    sentence.split.map {|word| word.reverse}.join(" ")
+end
